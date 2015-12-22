81e65e919374e061d4faa945a8371b76bffd1608
Alvaro Borrero <ajhades@hotmail.com>
2015-12-22 15:45:16 -0500

    cambios configuracion BD openshift

--- API/includes/config.php
+++ API/includes/config.php
@@ -2,9 +2,9 @@
 
 
   define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
-  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+  define( 'DB_USER', 'adminQMlSIdy' );             // Set database user
+  define( 'DB_PASS', 'zc-1ItInKGiw' );             // Set database password
+  define( 'DB_NAME', 'php' );        // Set database name
 
 
 ?>
